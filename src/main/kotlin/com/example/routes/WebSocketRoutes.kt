e27e52d23046cefbcef5abd6aebb812337bf615c
izowmart <izowmart8@gmail.com>
2021-10-18 07:09:47 +0300

    worked on drawData object in the room

--- src/main/kotlin/com/example/routes/WebSocketRoutes.kt
+++ src/main/kotlin/com/example/routes/WebSocketRoutes.kt
@@ -1,9 +1,13 @@
 package com.example.routes
 
+import com.example.data.Room
 import com.example.gson
 import com.example.models.BaseModel
 import com.example.models.ChatMessage
+import com.example.models.DrawData
 import com.example.other.Constants.TYPE_CHAT_MESSAGE
+import com.example.other.Constants.TYPE_DRAW_DATA
+import com.example.server
 import com.example.session.DrawingSession
 import com.google.gson.JsonParser
 import io.ktor.http.cio.ConnectionOptions.Companion.Close
@@ -14,6 +18,25 @@ import io.ktor.websocket.*
 import kotlinx.coroutines.channels.consume
 import kotlinx.coroutines.channels.consumeEach
 import java.lang.Exception
+
+fun Route.gameWebSocketRoute() {
+    route("/ws/draw") {
+        standardWebSocket { socket, clientId, message, payload ->
+            when (payload) {
+                is DrawData -> {
+                    val room = server.rooms[payload.roomName] ?: return@standardWebSocket
+                    if (room.phase == Room.Phase.GAME_RUNNING) {
+                        room.broadcastToAllExcept(message, clientId)
+                    }
+                }
+                is ChatMessage -> {
+
+                }
+            }
+
+        }
+    }
+}
 
 fun Route.standardWebSocket(
     handleFrame: suspend (
@@ -37,10 +60,11 @@ fun Route.standardWebSocket(
                     val jsonObject = JsonParser.parseString(message).asJsonObject
                     val type = when (jsonObject.get("type").asString) {
                         TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+                        TYPE_DRAW_DATA -> DrawData::class.java
                         else -> BaseModel::class.java
                     }
                     val payload = gson.fromJson(message, type)
-                    handleFrame(this,session.clientId,message,payload)
+                    handleFrame(this, session.clientId, message, payload)
                 }
             }
 
