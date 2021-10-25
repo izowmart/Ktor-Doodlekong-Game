8b2e0985586a2b8b53e2b8f01d1ec7476429fbe6
izowmart <izowmart8@gmail.com>
2021-10-25 07:00:01 +0300

    coroutine joba functionalities.

--- src/main/kotlin/com/example/data/Room.kt
+++ src/main/kotlin/com/example/data/Room.kt
@@ -57,7 +57,30 @@ class Room(
     }
 
     suspend fun addPlayer(clientId: String,username: String,socket:WebSocketSession): Player{
-        val player = Player(username,socket,clientId)
+        var indexToAdd = players.size - 1
+        val player = if(leftPlayers.contains(clientId)){
+            val leftPlayer = leftPlayers[clientId]
+            leftPlayer?.first?.let {
+                it.socket = socket
+                it.isDrawing = drawingPlayer?.clientId == clientId
+                indexToAdd = leftPlayer.second
+
+                playerRemoveJobs[clientId]?.cancel()
+                playerRemoveJobs.remove(clientId)
+                leftPlayers.remove(clientId)
+                it
+            } ?: Player(username, socket, clientId)
+        }else{
+            Player(username, socket, clientId)
+        }
+
+        indexToAdd = when{
+            players.isEmpty() -> 0
+            indexToAdd >= players.size ->players.size -1
+            else -> indexToAdd
+        }
+        val tmpPlayers = players.toMutableList()
+        tmpPlayers.add(indexToAdd, player)
         players = players + player
 
          if (players.size == 1){
