26cf038df9cbe8b3cb37ba2d1294aed6b72966ee
izowmart <izowmart8@gmail.com>
2021-10-28 06:39:16 +0300

    server project completed successfully

--- src/main/kotlin/com/example/data/Room.kt
+++ src/main/kotlin/com/example/data/Room.kt
@@ -30,6 +30,7 @@ class Room(
     private var leftPlayers = ConcurrentHashMap<String, Pair<Player, Int>>()
 
     private var curRoundDrawData: List<String> = listOf()
+    var lastDrawData: DrawData? = null
 
     private var phaseChangedListener: ((Phase) -> Unit)? = null
     var phase = Phase.WAITING_FOR_PLAYERS
@@ -66,6 +67,15 @@ class Room(
 
     fun addSerializedDrawInfo(drawAction: String){
         curRoundDrawData = curRoundDrawData + drawAction
+    }
+
+    private suspend fun finishOfDrawing(){
+        lastDrawData?.let {
+            if (curRoundDrawData.isNotEmpty() && it.motionEvent == 2){
+                val finishDrawData = it.copy(motionEvent = 1) // motionEvent 1 = action up and 2 = action down/drawing
+                broadcast(gson.toJson(finishDrawData))
+            }
+        }
     }
 
     suspend fun addPlayer(clientId: String,username: String,socket:WebSocketSession): Player{
@@ -170,9 +180,15 @@ class Room(
             }
             phase = when(phase){
                 Phase.WAITING_FOR_START -> Phase.NEW_ROUND
-                Phase.GAME_RUNNING -> Phase.SHOW_WORD
+                Phase.GAME_RUNNING -> {
+                    finishOfDrawing()
+                    Phase.SHOW_WORD
+                }
                 Phase.SHOW_WORD -> Phase.NEW_ROUND
-                Phase.NEW_ROUND -> Phase.GAME_RUNNING
+                Phase.NEW_ROUND -> {
+                    word = null
+                    Phase.GAME_RUNNING
+                }
                 else -> Phase.WAITING_FOR_PLAYERS
             }
         }
--- src/main/kotlin/com/example/routes/WebSocketRoutes.kt
+++ src/main/kotlin/com/example/routes/WebSocketRoutes.kt
@@ -56,6 +56,7 @@ fun Route.gameWebSocketRoute() {
                         room.broadcastToAllExcept(message, clientId)
                         room.addSerializedDrawInfo(message)
                     }
+                    room.lastDrawData = payload
                 }
                 is DrawAction -> {
                     val room = server.getRoomWithClientId(clientId) ?: return@standardWebSocket
